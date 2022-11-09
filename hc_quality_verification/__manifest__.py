6598659c2ea4e2f700afa4bb9a55cd3269b7ba8c
allwin <allwin@hashcodeit.com>
2022-11-09 12:01:13 +0530

    Added quality verification button, status bar for admin

---
+++ hc_quality_verification/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'Quality Verification',
+    "author": "Hashcode IT Solutions",
+    'website': 'https://hashcodeit.com/',
+    'version': '15.0.0.1',
+    'support': 'info@hashcodeit.com',
+    'category': '',
+    'summary': 'This module adds quality verification option for administrator',
+    'description': """ """,
+    'depends': ['stock'],
+    'data': ['views/stock_picking_view.xml'
+    ],
+    "images": [],
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ hc_quality_verification/models/quality_verification.py
@@ -0,0 +1,28 @@
+from odoo import api, models, fields, _
+
+
+class QualityVerification(models.Model):
+    _inherit = 'stock.picking'
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('waiting', 'Waiting Another Operation'),
+        ('confirmed', 'Waiting'),
+        ('assigned', 'Ready'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled'),
+        ('verified', 'Verified')
+    ], string='Status', compute='_compute_state',
+        copy=False, index=True, readonly=True, store=True, tracking=True,
+        help=" * Draft: The transfer is not confirmed yet. Reservation doesn't apply.\n"
+             " * Waiting another operation: This transfer is waiting for another operation before being ready.\n"
+             " * Waiting: The transfer is waiting for the availability of some products.\n(a) The shipping policy is \"As soon as possible\": no product could be reserved.\n(b) The shipping policy is \"When all products are ready\": not all the products could be reserved.\n"
+             " * Ready: The transfer is ready to be processed.\n(a) The shipping policy is \"As soon as possible\": at least one product has been reserved.\n(b) The shipping policy is \"When all products are ready\": all product have been reserved.\n"
+             " * Done: The transfer has been processed.\n"
+             " * Cancelled: The transfer has been cancelled.\n"
+             " * Verified:The transfer is verified by admin")
+
+    def action_verified(self):
+        self.state = 'verified'
+
+
